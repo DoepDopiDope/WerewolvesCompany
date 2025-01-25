38ba39fcf8adb49a325e3c90ad27547b1bac0727
DoepDopiDope <62354170+DoepDopiDope@users.noreply.github.com>
2025-01-25 01:02:06 +0100

    fixed package number

--- WerewolvesCompany/Plugin.cs
+++ WerewolvesCompany/Plugin.cs
@@ -23,7 +23,7 @@ namespace WerewolvesCompany
     {
         const string GUID = "doep.WerewolvesCompany";
         const string NAME = "WerewolvesCompany";
-        const string VERSION = "0.1.2";
+        const string VERSION = "0.1.3";
 
         internal static InputsClass InputActionsInstance;
 
